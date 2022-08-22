44b79caf9cf4d46def34c2ef1b470b9d0d2d1aaf
Zechariah <zechariahtan144@gmail.com>
2022-08-23 01:40:08 +0800

    ✨ stack trace example

---
+++ src/StackTraceExample.java
@@ -0,0 +1,19 @@
+public class StackTraceExample {
+	public static void main(String args[]) {
+		AnotherClass.firstCall();
+	}
+
+	public static void divideZero() {
+		int i = 0 / 0;
+	}
+}
+
+class AnotherClass {
+	public static void firstCall() {
+		secondCall();
+	}
+
+	public static void secondCall() {
+		StackTraceExample.divideZero();
+	}
+}
